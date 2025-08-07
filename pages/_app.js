052a08916dc37d224786a132a6a46046c4246f4a
omc324 <9977omc@gmail.com>
2025-08-07 17:34:07 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
+// pages/_app.js
 import '../styles/globals.css';
-/* In globals.css */
-body {
-  background-image: url('/bg.jpg');
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-  color: white;
-  min-height: 100vh;
-  margin: 0;
-  padding: 0;
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+
 
 
 
